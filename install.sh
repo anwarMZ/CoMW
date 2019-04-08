a5e2a6e998ec69a5ddd7f4385e1bde4ed5743adc
Muhammad Zohaib Anwar <mzanwar@envs.au.dk>
2019-04-08 16:22:27 +0200

    Update CoMW

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-printf "\n\nCloning and making Sword\n\n"
-#Cloning the sword into sword directory
-git clone --recursive https://github.com/rvaser/sword.git sword
-cd sword/
-make 
-
 printf "\n\nDownloading and making databases available for CoMW\n\n"
 #Downloading the databases and unzipping them
 wget -A zip -r -l 1 -nd -P ./databases/ https://mzacomw.au.dk
