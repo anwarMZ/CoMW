6c97295698685f9efca7fc3e4ee0877e751aa6ec
Muhammad Zohaib Anwar <mzanwar@envs.au.dk>
2019-05-02 08:59:38 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 
-printf "\n\nDownloading and making databases available for CoMW\n\n"
+#printf "\n\nDownloading and making databases available for CoMW\n\n"
 #Downloading the databases and unzipping them
-wget -A zip -r -l 1 -nd -P ./databases/ https://mzacomw.au.dk
-for i in ./databases/*.zip
-do 
-	unzip -o $i -d ./databases/
-	rm -rf $i
-done
+#wget -A zip -r -l 1 -nd -P ./databases/ https://mzacomw.au.dk
+#for i in ./databases/*.zip
+#do 
+#	unzip -o $i -d ./databases/
+#	rm -rf $i
+#done
+
+
+printf "\n\nDownloading and installing sword for CoMW\n\n"
+git clone --recursive https://github.com/rvaser/sword.git sword
+cd sword/
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make
+
 printf "\n\nCoMW installed, ready to use."
